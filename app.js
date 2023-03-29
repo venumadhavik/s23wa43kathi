81f51ad2a4761649673dd51f329f0faab154e145
venumadhavik <123043484+venumadhavik@users.noreply.github.com>
2023-03-29 18:53:05 -0500

    new endpoint

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mydataRouter = require('./routes/mydata');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydataRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
