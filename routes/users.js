8fa319c3f48df0ede30cce975a35d1ad23b0faac
venumadhavik <123043484+venumadhavik@users.noreply.github.com>
2023-03-29 19:03:08 -0500

    new endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+let sum = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  sum = sum+1
+  res.send('sum is: '+sum);
 });
 
 module.exports = router;
